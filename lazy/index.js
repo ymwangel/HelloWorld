c224a1890cb305635f7e28c93b3a59d7a45af3ba
yanmin.wang <yanmin.wang@wuage.com>
2018-04-27 23:01:00 +0800

    lazyimg

--- lazy/index.js
+++
@@ -1,6 +0,0 @@
-import LazyImg from './lib/lazy-img'
-
-new LazyImg({
-  container: 'body',
-  dataSrc: 'data-src'
-})
--- lazy/umd-lazy-img.js
+++ lazy/umd-lazy-img.js
@@ -1,6 +1,5 @@
 /*
  *
- *  img-jquery
  *  参数说明
  *  container:传入的dom元素内执行懒加载 不传默认是body,如果是id属性，则传入‘#id’，class属性，则传入‘.class’
  *  dataSrc: 懒加载需要的图片的key <img data-src="xxx.gif" />
